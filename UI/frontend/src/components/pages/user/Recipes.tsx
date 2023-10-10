93a3676354711fee44e7bc31783c60a9d9779f5b
Jacob Crewe <jacobdc0602@gmail.com>
2023-10-09 22:17:11 -0400

    Added kind of base layout for recipes to help. Added some base layout for dashboard

--- UI/frontend/src/components/pages/user/Recipes.tsx
+++ UI/frontend/src/components/pages/user/Recipes.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Recipes() {
   return (
     <div className="w-full h-full">
-      <p className="text-7xl">Recipes Page</p>
+      <div className="flex flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 ">
+        <p className="text-7xl">Recipes Page</p>
+      </div>
     </div>
   );
 }
