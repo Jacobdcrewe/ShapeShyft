f6645a59b361bc34852ba194e6d07fd127154b64
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2023-10-29 14:09:39 -0400

    Adjusted Food and Calories page to match theme

--- UI/frontend/src/components/pages/user/FoodAndCalories.tsx
+++ UI/frontend/src/components/pages/user/FoodAndCalories.tsx
@@ -27,6 +27,36 @@ export function FoodAndCalories() {
       calories: 250,
       mealType: "Breakfast",
     },
+    {
+      // example cases
+      name: "Mansaf",
+      amount: 10,
+      unit: "cups",
+      calories: 2000,
+      mealType: "Breakfast",
+    },
+    {
+      name: "Cheese",
+      amount: 10,
+      unit: "grams",
+      calories: 250,
+      mealType: "Breakfast",
+    },
+    {
+      // example cases
+      name: "Mansaf",
+      amount: 10,
+      unit: "cups",
+      calories: 2000,
+      mealType: "Breakfast",
+    },
+    {
+      name: "Cheese",
+      amount: 10,
+      unit: "grams",
+      calories: 250,
+      mealType: "Breakfast",
+    },
   ]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [newFoodItem, setNewFoodItem] = useState<FoodItemProps>({
@@ -61,14 +91,7 @@ export function FoodAndCalories() {
   };
 
   return (
-    <div className="w-full h-full">
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        onClick={openModal}
-      >
-        Add Food Item
-      </button>
-
+    <div className="w-full h-full p-5">
       {/* Modal */}
       <Modal
         isOpen={modalIsOpen}
@@ -151,7 +174,7 @@ export function FoodAndCalories() {
           </select>
 
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+            className="bg-violet-800 text-white px-4 py-2 rounded mt-4"
             onClick={handleAddFoodItem}
           >
             Add Food Item
@@ -159,29 +182,39 @@ export function FoodAndCalories() {
         </div>
       </Modal>
 
-      <div className="p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5 ">
         {["Breakfast", "Lunch", "Dinner", "Snacks"].map((mealType) => (
-          <div className="p-4" key={mealType}>
-            <div className="flex justify-between">
+          <div className="rounded-xl h-96 px-4 py-2 bg-neutral-50 overflow-hidden shadow-[0px 0px 10px rgba(0,0,0,0.2)]" key={mealType}>
+            <div className="flex justify-between mb-4">
               <h1 className="text-2xl font-semibold">{mealType}</h1>
               <span className="text-2l font-bold">
                 {calculateTotalCalories(mealType)} cal
               </span>
             </div>
-            {foodItems
-              .filter((item) => item.mealType === mealType)
-              .map((item, index) => (
-                <FoodItem
-                  key={index}
-                  name={item.name}
-                  amount={item.amount}
-                  unit={item.unit}
-                  calories={item.calories}
-                  mealType={item.mealType}
-                />
-              ))}
+            <div className="overflow-y-auto h-72"> {/* Changes made here */}
+              {foodItems
+                .filter((item) => item.mealType === mealType)
+                .map((item, index) => (
+                  <FoodItem
+                    key={index}
+                    name={item.name}
+                    amount={item.amount}
+                    unit={item.unit}
+                    calories={item.calories}
+                    mealType={item.mealType}
+                  />
+                ))}
+            </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center p-10 ">
+        <button
+          className="shadow-[0px_0px_10px_rgba(0,0,0,0.4)] hover:shadow-[0px_0px_10px_rgba(0,0,0,0.6)] whitespace-nowrap rounded-full text-white w-1/8 bg-violet-800 brightness-[0.95] hover:brightness-100 px-6 py-2 scale-100 hover:scale-[1.01] transition-transform ease-in-out duration-150"
+          onClick={openModal}
+        >
+          Add Food Item
+        </button>
       </div>
     </div>
   );
