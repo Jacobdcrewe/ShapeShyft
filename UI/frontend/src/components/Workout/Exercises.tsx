7e13dea3dd3bbffda2d607149bc4e95d8fced248
Rawan <rawan.r.karroum@gmail.com>
2023-10-30 13:11:56 -0400

    fetched first name to display it at the top of the page and fixed some stuff

--- UI/frontend/src/components/Workout/Exercises.tsx
+++ UI/frontend/src/components/Workout/Exercises.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Exercises = () => {
-  return <div>Exercises</div>;
+  return (
+    <div className="absolute mt-12 mb-8 left-16">
+      <div className="text-4xl font-bold">
+        Showing Results
+      </div>
+    </div>
+  )
+  
 };
 
 export default Exercises;
