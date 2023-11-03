c8dfd38aef0181cac08aaa28307fbcc454f054ef
Jacob Crewe <jacobdc0602@gmail.com>
2023-11-03 12:56:00 -0400

    Made pie chart responsive. Added exercise

--- UI/frontend/src/components/pages/user/Dashboard.tsx
+++ UI/frontend/src/components/pages/user/Dashboard.tsx
@@ -1,24 +1,16 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../ContentRouter";
 import { GET } from "../../../composables/api";
 import file from "../../../composables/urls.json";
-import { FireIcon, HeartIcon } from "@heroicons/react/24/solid";
-import CaloryItem from "../../dashboard/CaloryItem";
-import Box from "@mui/material/Box";
-import { Cell, Pie, PieChart } from "recharts";
-import Loading from "../../common/Loading";
-import {
-  EnvelopeIcon,
-  FingerPrintIcon,
-  UserIcon,
-} from "@heroicons/react/24/outline";
+import UserInfoSection from "../../dashboard/UserInfoSection";
+import CalorySection from "../../dashboard/calory/CalorySection";
+import ExerciseSection from "../../dashboard/ExerciseSection";
+import HealthandWellnessSection from "../../dashboard/HealthandWellnessSection";
 
 export function Dashboard() {
   const [exuser, setExUser] = useState(
     "(example of making api call) click me!"
   );
-  const [user, setUser] = useState({} as any);
-  const [loading, setLoading] = useState(true);
 
   const { login } = useContext(UserContext);
   const exampleFunction = async () => {
@@ -26,138 +18,23 @@ export function Dashboard() {
     setExUser(JSON.stringify(val));
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const val = await GET(file.me, login);
-      setUser(val);
-      setLoading(false);
-    };
-    fetchData();
-  }, [login]);
-
-  let caloryItem = {
-    caloryIntake: 3240,
-    caloryBurn: 500,
-  };
-
-  let caloryItems = [
-    {
-      icon: <FireIcon className="w-full h-full text-violet-700" />,
-      text: "Calories Burned",
-      value: caloryItem.caloryBurn,
-    },
-    {
-      icon: <HeartIcon className="w-full h-full text-rose-700" />,
-      text: "Calories Consumed",
-      value: caloryItem.caloryIntake,
-    },
-  ];
-
-  const data = [
-    {
-      name: "Calories Burned",
-      value: caloryItem.caloryBurn,
-    },
-    {
-      name: "Calories Consumed",
-      value: caloryItem.caloryIntake,
-    },
-  ];
-  const COLORS = ["rgb(109 40 217)", "rgb(190 18 60)"];
-
   return (
     <div className="w-full h-full min-w-[330px] overscroll-x-contain">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div className="rounded-xl p-4 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] md:col-span-2">
-          <div className="flex flex-col sm:flex-row flex-wrap h-full sm:gap-y-1 gap-x-8 text-lg sm:text-3xl">
-            <div className="flex items-center min-h-[50px] min-w-[200px]">
-              <FingerPrintIcon className="aspect-square h-6 w-6 mr-3 flex-none text-violet-700" />
-              {loading ? (
-                <div className="mb-2">
-                  <Loading />
-                </div>
-              ) : (
-                <p>
-                  {user.first_name} {user.last_name}
-                </p>
-              )}
-            </div>
-            <div className="flex items-center min-h-[50px] min-w-[200px]">
-              <UserIcon className="aspect-square h-6 w-6 mr-3 flex-none text-violet-700" />
-              {loading ? (
-                <div className="mb-2">
-                  <Loading />
-                </div>
-              ) : (
-                <p>{user.phone_number}</p>
-              )}
-            </div>
-            <div className="flex items-center min-h-[50px] min-w-[200px]">
-              <EnvelopeIcon className="aspect-square flex-none h-6 w-6 mr-3 mt-2 text-violet-700" />
-              {loading ? (
-                <div className="mb-2">
-                  <Loading />
-                </div>
-              ) : (
-                <p>{user.email}</p>
-              )}
-            </div>
-          </div>
+          <UserInfoSection />
         </div>
         <div className="rounded-xl col-span-1 md:col-span-2 p-4 bg-white shadow-[0px_0px_10px_rgba(0,0,0,0.2)] flex flex-col md:flex-row flex-wrap ">
-          <div className="w-full md:w-auto flex items-center justify-center mr-10">
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-evenly",
-                flexWrap: "wrap",
-              }}
-            >
-              <PieChart width={380} height={380}>
-                <Pie
-                  data={data}
-                  innerRadius={100}
-                  outerRadius={140}
-                  cornerRadius={15}
-                  paddingAngle={5}
-                  dataKey="value"
-                >
-                  {data.map((entry, index) => (
-                    <Cell
-                      className="hover:animate-pulse"
-                      style={{ outline: "none" }}
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
-                  ))}
-                </Pie>
-              </PieChart>
-            </Box>
-          </div>
-
-          <div className="gap-4 flex flex-col justify-center">
-            {caloryItems.map((val: any) => {
-              return (
-                <div key={val.text} className="hover:animate-pulse">
-                  <CaloryItem
-                    icon={val.icon}
-                    text={val.text}
-                    value={val.value}
-                  />
-                </div>
-              );
-            })}
-          </div>
+          <CalorySection />
         </div>
-        <div className="rounded-xl h-32 px-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] ">
-          <p className="text-3xl">Exercise</p>
+        <div className="rounded-xl p-4 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] col-span-1 md:col-span-2 xl:col-span-1 min-h-[270px]">
+          <ExerciseSection />
         </div>
-        <div className="rounded-xl h-32 px-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] ">
-          <p className="text-3xl">Sleep/Water?</p>
+        <div className="rounded-xl h-32 p-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] col-span-1 md:col-span-2 xl:col-span-1">
+          <HealthandWellnessSection />
         </div>
         <div
-          className="rounded-xl h-32 px-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] hover:cursor-pointer md:col-span-2"
+          className="rounded-xl h-32 p-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] hover:cursor-pointer md:col-span-2"
           onClick={exampleFunction}
         >
           <p>{exuser}</p>
