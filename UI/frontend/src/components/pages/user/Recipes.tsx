4fc7e2d20ac053353326b855d82f41177683f480
Jacob Crewe <jacobdc0602@gmail.com>
2023-11-26 22:30:45 -0500

    added recipes changes

--- UI/frontend/src/components/pages/user/Recipes.tsx
+++ UI/frontend/src/components/pages/user/Recipes.tsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Popup from "../../recipes/Popup";
+import { UserContext } from "../../ContentRouter";
+import { GET } from "../../../composables/api";
+import file from "../../../composables/urls.json";
+
+interface Recipe {
+  name: string;
+  image: string;
+  description: string;
+  ingredients: string;
+  instructions: string;
+  nutInfo: {
+    cals: string;
+    carbs: string;
+    fat: string;
+    protein: string;
+  };
+}
 
 export function Recipes() {
+  const { login } = useContext(UserContext);
+
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(false);
   const [popupOpen, setPopupOpen] = useState(false);
-  const [selectedRecipeIndex, setSelectedRecipeIndex] = useState<number | null>(
-    null
-  ); // Initialize as null
+  const [selectedRecipeIndex, setSelectedRecipeIndex] = useState<number | null>(null);
   const [searchText, setSearchText] = useState("");
 
   const openPopup = (index: number) => {
@@ -15,58 +34,74 @@ export function Recipes() {
 
   const closePopup = () => {
     setPopupOpen(false);
-    setSelectedRecipeIndex(null); // Reset the selectedRecipeIndex when closing the popup
+    setSelectedRecipeIndex(null);
   };
 
-  const handleSearchTextChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleSearchTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
   };
+
+  const handleSearch = async () => {
+    try {
+      setLoading(true);
+      const response = await GET(file.getRecipe + "?query=" + searchText, login);
+      setRecipes(response.items);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
 
   return (
     <div className="w-full h-full">
       <div className="flex flex-row justify-between items-center mb-5">
         <h1 className="text-3xl font-bold">Recipes</h1>
-        <input
-          type="text"
-          placeholder="Find a Recipe"
-          value={searchText}
-          onChange={handleSearchTextChange}
-          className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-        />
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Find a Recipe"
+            value={searchText}
+            onChange={handleSearchTextChange}
+            className="px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent mr-2"
+          />
+          <button
+            onClick={handleSearch}
+            className="px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer"
+          >
+            Search
+          </button>
+        </div>
       </div>
-      <div className="flex flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
-        {Array(25)
-          .fill(true)
-          .map((item, index) => {
-            if (
-              searchText.length > 0 &&
-              `Recipe ${index}`
-                .toLowerCase()
-                .indexOf(searchText.toLowerCase()) === -1
-            ) {
-              return null;
-            }
-            return (
-              <div
-                key={index}
-                onClick={() => openPopup(index)}
-                className="rounded-xl aspect-square px-4 py-2 bg-white overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] m-1 hover:m-0 transition-all ease-in-out duration-150 brightness-[0.98] hover:brightness-100 cursor-pointer"
-              >
-                <img
-                  src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D"
-                  alt="recipe"
-                  className="w-full h-40 object-cover rounded-md"
-                />
-                <p className="text-4xl">{`Recipe ${index}`}</p>
+      {loading ? (
+        <div className="text-center text-gray-600">Loading recipes...</div>
+      ) : recipes.length === 0 ? (
+        <div className="text-center text-gray-600">No recipes found.</div>
+      ) : (
+        <div className="flex flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+          {recipes.map((recipe, index) => (
+            <div
+              key={index}
+              onClick={() => openPopup(index)}
+              className="rounded-xl overflow-hidden shadow-[0px_0px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_0px_15px_rgba(0,0,0,0.3)] cursor-pointer bg-gray-300 flex flex-col"
+            >
+              <img
+                src={recipe.image}
+                alt={recipe.name}
+                className="w-full h-40 object-cover rounded-t-md"
+              />
+              <div className="bg-opacity-80 bg-black p-2 text-white flex-grow flex flex-col justify-center">
+                <div className="text-base font-bold text-center">{recipe.name}</div>
               </div>
-            );
-          })}
-      </div>
-
-      {/* Render the Popup component */}
-      {popupOpen && <Popup onClose={closePopup} index={selectedRecipeIndex} />}
+            </div>
+          ))}
+        </div>
+      )}
+      {popupOpen && <Popup onClose={closePopup} index={selectedRecipeIndex} recipes={recipes} />}
     </div>
   );
 }
